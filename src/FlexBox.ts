0a883c8cdd089c1c2f044ba81db2c8d6f7d7055b
bacebu4 <vasua14735@icloud.com>
2021-03-15 08:57:20 +0300

    feat: flexbox added

---
+++ src/FlexBox.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+interface FlexBoxProps {
+  jc?:
+    | "space-around"
+    | "space-between"
+    | "space-evenly"
+    | "stretch"
+    | "center"
+    | "end"
+    | "flex-end"
+    | "flex-start";
+  ai?:
+    | "center"
+    | "end"
+    | "flex-end"
+    | "flex-start"
+    | "self-end"
+    | "self-start"
+    | "start"
+    | "baseline"
+    | "normal"
+    | "stretch";
+  mt?: number;
+  ml?: number;
+  pr?: number;
+  w?: string;
+  h?: string;
+  margin?: string;
+  direction?: "column";
+}
+
+export const FlexBox = styled.div<FlexBoxProps>`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  justify-content: ${(props) => props.jc};
+  align-items: ${(props) => props.ai};
+  margin-left: ${(props) => props.ml}px;
+  margin-top: ${(props) => props.mt}px;
+  margin: ${(props) => props.margin};
+  padding-right: ${(props) => props.pr}px;
+  width: ${(props) => props.w};
+  height: ${(props) => props.h};
+`;
