86352734ac5db91b26be683b1140090c0b9099d2
bacebu4 <vasua14735@icloud.com>
2021-03-17 08:05:59 +0300

    feat: all margins props are present and no overwrites

--- src/LayoutStyles.ts
+++ src/LayoutStyles.ts
@@ -4,14 +4,38 @@ import styled, { StyledComponent } from "styled-components";
 type LayoutStylesProps = {
   mt?: number;
   ml?: number;
+  mr?: number;
+  mb?: number;
 };
 
 export function withLayoutStyles<P>(
   Component: React.FC<P>
 ): StyledComponent<React.FC<P>, object, P & LayoutStylesProps, never> {
   const ComponentWithAddedProps = styled(Component)<LayoutStylesProps & P>`
-    margin-top: ${(props) => props.mt}px;
-    margin-left: ${(props) => props.ml}px;
+    ${(props) => {
+      if (props.mt) {
+        return `margin-top: ${props.mt}px;`;
+      }
+      return "";
+    }}
+    ${(props) => {
+      if (props.mb) {
+        return `margin-bottom: ${props.mb}px;`;
+      }
+      return "";
+    }}
+    ${(props) => {
+      if (props.ml) {
+        return `margin-left: ${props.ml}px;`;
+      }
+      return "";
+    }}
+    ${(props) => {
+      if (props.mr) {
+        return `margin-right: ${props.mr}px;`;
+      }
+      return "";
+    }}
   `;
 
   return ComponentWithAddedProps;
--- tests/LayoutStyles.test.js
+++ tests/LayoutStyles.test.js
@@ -6,6 +6,7 @@ import { withLayoutStyles } from "../src";
 
 const StyledButton = styled.button`
   padding: 12px 16px;
+  margin-top: 9px;
 `;
 
 const FunctionComponentButton = ({ className }) => {
@@ -21,21 +22,37 @@ describe("withLayoutStyles", () => {
     const ModifiedComponent = withLayoutStyles(StyledButton);
     const tree = renderer
       .create(
-        <ModifiedComponent mt={10} ml={10}>
+        <ModifiedComponent mt={10} ml={11} mr={12} mb={13}>
           Hey
         </ModifiedComponent>
       )
       .toJSON();
     expect(tree).toHaveStyleRule("margin-top", "10px");
-    expect(tree).toHaveStyleRule("margin-left", "10px");
+    expect(tree).toHaveStyleRule("margin-left", "11px");
+    expect(tree).toHaveStyleRule("margin-right", "12px");
+    expect(tree).toHaveStyleRule("margin-bottom", "13px");
   });
 
   it("correctly works with FC", () => {
     const ModifiedComponent = withLayoutStyles(FunctionComponentButton);
     const tree = renderer
-      .create(<ModifiedComponent mt={10} ml={10} />)
+      .create(<ModifiedComponent mt={10} ml={11} mr={12} mb={13} />)
       .toJSON();
     expect(tree).toHaveStyleRule("margin-top", "10px");
-    expect(tree).toHaveStyleRule("margin-left", "10px");
+    expect(tree).toHaveStyleRule("margin-left", "11px");
+    expect(tree).toHaveStyleRule("margin-right", "12px");
+    expect(tree).toHaveStyleRule("margin-bottom", "13px");
+  });
+
+  it("does not overrides what was not used", () => {
+    const ModifiedComponent = withLayoutStyles(StyledButton);
+    const tree = renderer
+      .create(
+        <ModifiedComponent ml={11} mr={12} mb={13}>
+          Hey
+        </ModifiedComponent>
+      )
+      .toJSON();
+    expect(tree).toHaveStyleRule("margin-top", "9px");
   });
 });
